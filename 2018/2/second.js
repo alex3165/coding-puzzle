5045233a692067181f086f4da6dfd81515ef2649
Alexandre Rieux <alexr.3165@gmail.com>
2019-01-04 17:19:51 +0000

    Add day 2 second and day 2 first

---
+++ 2018/2/second.js
@@ -0,0 +1,51 @@
+const { read } = require("../../util");
+
+const input = read("./second-input.txt");
+
+const distance = (a, b) => {
+  let count = 0;
+
+  for (let i = 0; i < a.length; i++) {
+    const achar = a[i];
+    const bchar = b[i];
+    if (achar !== bchar) {
+      count++;
+    }
+  }
+
+  return count;
+};
+
+const removeChar = (a, b) => {
+  let res = "";
+
+  for (let i = 0; i < a.length; i++) {
+    const achar = a[i];
+    const bchar = b[i];
+    if (achar === bchar) {
+      res += achar;
+    }
+  }
+
+  return res;
+};
+
+input
+  .then(content => {
+    const pair = [];
+    for (let i = 0; i < content.length; i++) {
+      for (let j = content.length - 1; j >= 0; j--) {
+        if (i !== j && distance(content[i], content[j]) === 1) {
+          pair.push(content[i]);
+          pair.push(content[j]);
+          break;
+        }
+      }
+      if (pair.length) {
+        break;
+      }
+    }
+
+    return removeChar(...pair);
+  })
+  .then(console.log);
