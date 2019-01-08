dc7b23ad0e92c89a36086d0a47f11aa264c5726d
Alexandre Rieux <alexr.3165@gmail.com>
2019-01-08 09:08:23 +0000

    Improve day 4 algorithm

--- 2018/4/first.js
+++ 2018/4/first.js
@@ -1,13 +1,8 @@
 const { read } = require("../../util");
 const { parseDedupe } = require("./util");
 const input = read("./first-input.txt");
-const moment = require("moment");
 const { range } = require("../../util");
-
-const getDateKey = date =>
-  moment(date)
-    .add(30, "minutes")
-    .format("YYYY-MM-DD");
+const moment = require("moment");
 
 const mode = arr =>
   arr
@@ -17,55 +12,43 @@ const mode = arr =>
     )
     .pop();
 
-const groupRecords = records => {
-  return records.reduce((grouped, [date, instruction]) => {
-    if (Array.isArray(grouped[getDateKey(date)])) {
-      grouped[getDateKey(date)].push([date, instruction]);
-    } else {
-      grouped[getDateKey(date)] = [[date, instruction]];
-    }
-
-    return grouped;
-  }, {});
-};
-
 const extractId = string =>
   parseInt(string.split(/Guard #(.+) begins shift/)[1]);
 
 input
   .then(content => parseDedupe(content))
-  .then(records => groupRecords(records))
   .then(records => {
     const mapGuardIdToTime = {};
+    let currentGuardId;
 
-    Object.values(records).forEach(instructions => {
-      const guardId = extractId(instructions.shift()[1]);
-
-      for (let i = 0; i < instructions.length; i = i + 2) {
-        const sleep = moment(instructions[i][0])
-          .utc()
-          .minutes();
-        const awake = moment(instructions[i + 1][0])
-          .utc()
-          .minutes();
-
-        const diff = awake - sleep;
-        const currentRange = range(diff, sleep);
-
-        if (mapGuardIdToTime[guardId]) {
-          mapGuardIdToTime[guardId].total += diff;
-          mapGuardIdToTime[guardId].range = mapGuardIdToTime[
-            guardId
-          ].range.concat(currentRange);
-        } else {
-          mapGuardIdToTime[guardId] = {
-            total: diff,
-            id: guardId,
-            range: currentRange
-          };
-        }
+    while (!!records.length) {
+      if (records[0][1].includes("Guard")) {
+        currentGuardId = extractId(records.shift()[1]);
       }
-    });
+
+      const sleep = moment(records.shift()[0])
+        .utc()
+        .minutes();
+      const awake = moment(records.shift()[0])
+        .utc()
+        .minutes();
+
+      const diff = awake - sleep;
+      const currentRange = range(diff, sleep);
+
+      if (mapGuardIdToTime[currentGuardId]) {
+        mapGuardIdToTime[currentGuardId].total += diff;
+        mapGuardIdToTime[currentGuardId].range = mapGuardIdToTime[
+          currentGuardId
+        ].range.concat(currentRange);
+      } else {
+        mapGuardIdToTime[currentGuardId] = {
+          total: diff,
+          id: currentGuardId,
+          range: currentRange
+        };
+      }
+    }
 
     const sorted = Object.values(mapGuardIdToTime).sort((a, b) =>
       a.total > b.total ? -1 : 1
--- 2018/4/util.js
+++ 2018/4/util.js
@@ -1,33 +1,30 @@
 const moment = require("moment");
 
 const parseDedupe = content => {
-  const input = content
+  return content
     .map(val => {
       const res = val.split(/\[(.*)\] /g);
       res.shift();
       const date = moment(res[0], "YYYY-MM-DD hh:mm");
       return [date, res[1]];
     })
-    .sort((a, b) => {
-      return a[0].isBefore(b[0]) ? -1 : 1;
-    })
-    .map(arr => [arr[0].format(), arr[1]]);
+    .sort((a, b) => (a[0].isBefore(b[0]) ? -1 : 1))
+    .map(arr => [arr[0].format(), arr[1]])
+    .reduce((acc, next) => {
+      if (!acc.length) {
+        acc.push(next);
+        return acc;
+      }
 
-  return input.reduce((acc, next) => {
-    if (!acc.length) {
+      const lastTuple = acc[acc.length - 1];
+      if (lastTuple[1].includes("Guard") && next[1].includes("Guard")) {
+        acc.pop();
+      }
+
       acc.push(next);
+
       return acc;
-    }
-
-    const lastTuple = acc[acc.length - 1];
-    if (lastTuple[1].includes("Guard") && next[1].includes("Guard")) {
-      acc.pop();
-    }
-
-    acc.push(next);
-
-    return acc;
-  }, []);
+    }, []);
 };
 
 module.exports = {
